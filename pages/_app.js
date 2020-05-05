94002a7076233cca2a2424d4f1d7b7d4d4f75e0d
Youmin Kim <powereyes@gmail.com>
2020-05-05 23:28:10 +0800

    init

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '@shopify/polaris/styles.css';
+import {AppProvider} from '@shopify/polaris';
+import translations from '@shopify/polaris/locales/en.json';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <React.Fragment>
+        <AppProvider i18n={translations}>
+            <Component {...pageProps} />
+        </AppProvider>
+    </React.Fragment>
+  )
+}
